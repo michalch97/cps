8055ca31c28298497f5aadbf4dbb62837f75e8e6
Micha? <michal.ch97@gmail.com>
2019-03-21 11:47:10 +0100

    [Add] Project initialization

---
+++ src/main/java/cps/Controller.java
@@ -0,0 +1,4 @@
+package cps;
+
+public class Controller {
+}
---
+++ src/main/java/cps/Main.java
@@ -0,0 +1,26 @@
+package cps;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        URL resource = getClass().getClassLoader().getResource("fxml/sample.fxml");
+        Parent root = FXMLLoader.load(resource);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
